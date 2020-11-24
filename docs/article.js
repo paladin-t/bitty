a7defb62ea3c7d295b82474910c888debaf8e1f7
Wang Renxin <hellotony521@qq.com>
2020-11-24 15:43:24 +0800

    *updated documents.

---
+++ docs/article.js
@@ -0,0 +1,61 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['exports'], factory);
+  } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+    factory(exports);
+  } else {
+    factory(root.Article = { });
+  }
+} (this, function (exports) {
+  function render (markdown) {
+    return Promise.resolve(marked(markdown));
+  }
+
+  function article (url) {
+    return fetch(url, {
+      method: 'GET',
+      mode: 'cors',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    }).then(function (rsp) {
+      return rsp.text();
+    });
+  }
+
+  function attach (node, html) {
+    if (typeof node == 'string')
+      node = document.getElementById(node);
+    node.innerHTML = html;
+
+    return node;
+  }
+
+  function read (url, ref, nodes) {
+    return article(url)
+      .then(function (markdown) {
+        markdown = nodes.parse(markdown);
+
+        return render(markdown)
+          .then(function (html) {
+            html = nodes.preprocess(html);
+
+            attach(nodes.content, html);
+
+            nodes.highlight();
+          });
+      })
+      .catch(function (_) {
+        var html = 'Oops, cannot load content for the moment...<br>Try refersh or ';
+        html += '<a href="' + ref + '" target="_blank">click</a>';
+
+        attach(nodes.content, html);
+      });
+  }
+
+  exports.render = render;
+  exports.article = article;
+  exports.attach = attach;
+  exports.read = read;
+}));
---
+++ docs/prism-lua.js
@@ -0,0 +1,30 @@
+Prism.languages.lua = {
+  'comment': [
+    {
+      pattern: /(^|[^\\])\-\-\[\[[\s\S]*?(?:\]\]|$)/,
+      lookbehind: true
+    },
+    {
+      pattern: /(^|[^\\:])\-\-.*/,
+      lookbehind: true,
+      greedy: true
+    }
+  ],
+  'string': {
+    pattern: /(["'])(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
+    greedy: true
+  },
+  'class-name': {
+    pattern: /(\b(?:class|new)\s+|\bcatch\s+\()[\w.\\]+/i,
+    lookbehind: true,
+    inside: {
+      'punctuation': /[.\\]/
+    }
+  },
+  'keyword': /\b(?:and|break|do|else|elseif|end|for|function|goto|if|in|local|nil|not|or|repeat|return|then|until|while)\b/,
+  'boolean': /\b(?:true|false)\b/,
+  'function': /\w+(?=\()/,
+  'number': /\b0x[\da-f]+\b|(?:\b\d+\.?\d*|\B\.\d+)(?:e[+-]?\d+)?/i,
+  'operator': /[<>]=?|[!=]=?=?|--?|\+\+?|&&?|\|\|?|[?*/~^%]/,
+  'punctuation': /[{}[\];(),.:]/
+};
