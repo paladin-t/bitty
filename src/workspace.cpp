c57fc554f83ee19b233ccb7048ab90282cf0e52f
Wang Renxin <hellotony521@qq.com>
2021-06-02 16:13:41 +0800

    *Fixed a canvas size injection issue of effects.

--- src/workspace.cpp
+++ src/workspace.cpp
@@ -1723,16 +1723,16 @@ bool Workspace::canvas(class Window* wnd, class Renderer* rnd, const class Proje
 		} else {
 			times = std::max((float)std::floor(rnd->height() / BITTY_CANVAS_DEFAULT_HEIGHT) - 1, 1.0f);
 		}
-		const bool tobeValidated =
-			(cvsSize.x > 0 && cvsSize.y > 0) &&
-			(
-				(canvasValidation() != Math::Vec2i(-1, -1) && canvasValidation() != cvsSize) ||
-				((num1 || num2 || num3 || num4) && modifier)
-			);
+		const bool fixedSize = cvsSize.x > 0 && cvsSize.y > 0;
+		const bool diffSize = canvasValidation() != Math::Vec2i(-1, -1) && canvasValidation() != cvsSize;
+		const bool scaledSize = (num1 || num2 || num3 || num4) && modifier;
+		const bool tobeValidated = fixedSize && (diffSize || scaledSize);
 		if (tobeValidated) {
 			cond = ImGuiCond_Always;
 
 			canvasValidation(cvsSize);
+		} else if (diffSize) {
+			canvasValidation(Math::Vec2i(std::abs(cvsSize.x), std::abs(cvsSize.y)));
 		}
 		ImVec2 wndSize;
 		ImVec2 wndMinSize;
