aaddd83fd2baea5a1f0aba552dd5d7f447cd8aa3
Wang Renxin <hellotony521@qq.com>
2022-07-13 21:27:56 +0800

    +Added a `Jpath::count(...)` function.

--- lib/jpath/jpath.hpp
+++ lib/jpath/jpath.hpp
@@ -3,7 +3,7 @@
 **
 ** For the latest info, see https://github.com/paladin-t/jpath
 **
-** Copyright (C) 2020 - 2021 Tony Wang
+** Copyright (C) 2020 - 2022 Tony Wang
 **
 ** Permission is hereby granted, free of charge, to any person obtaining a copy
 ** of this software and associated documentation files (the "Software"), to deal
@@ -291,6 +291,18 @@ template<typename ...Path> bool has(const rapidjson::Value &obj, Path ...path) {
 		return false;
 
 	return true;
+}
+template<typename ...Path> int count(const rapidjson::Value &obj, Path ...path) {
+	const rapidjson::Value* tmp = nullptr;
+	if (!read(obj, tmp, path ...))
+		return 0;
+	if (!tmp)
+		return 0;
+
+	if (!tmp->IsArray())
+		return 0;
+
+	return (int)tmp->GetArray().Size();
 }
 template<typename Ret, typename ...Path> bool get(const rapidjson::Value &obj, Ret &ret, Path ...path) {
 	const rapidjson::Value* tmp = nullptr;
