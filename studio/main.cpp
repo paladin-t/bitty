ce84dfc6c76dc3bcff7805681fd4f6d1ef1d81b7
Tony Wang <hellotony@vista.aero>
2026-03-31 18:11:53 +0800

    *2026.

--- studio/main.cpp
+++ studio/main.cpp
@@ -3,7 +3,7 @@
 **
 ** An itty bitty 2D game engine.
 **
-** Copyright (C) 2020 - 2025 Tony Wang, all rights reserved
+** Copyright (C) 2020 - 2026 Tony Wang, all rights reserved
 **
 ** For the latest info, see https://github.com/paladin-t/bitty/
 */
